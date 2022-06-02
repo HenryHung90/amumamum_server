0a2eeb380aff94daba6234eed87c2cbf6ab37751
上傳人名字 <hungwork510031@gmail.com>
2022-06-03 02:23:37 +0800

    6/2

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const bcryptjs = require("bcrypt");
+//dialogflowAgent使用
+const dialogflowAgent = require("dialogflow-fulfillment");
 //引入Schema
 const SignUpTemplateCopy = require("../models/SignUpModels");
 const MarkUserTemplateCopy = require("../models/MarkUserModels");
+const UserNoteTemplateCopy = require("../models/UserNoteModels");
 
 /////////////寫入資料庫///////////////////
 router.post(process.env.ROUTER_SIGNUP, async (req, res) => {
@@ -59,26 +62,48 @@ router.get(process.env.ROUTER_READ, async (req, res) => {
   ////////////////////////////////
 });
 /////////////Dialogflow///////////////////
-router.post(process.env.ROUTER_DIALOGFLOW, (req, res, next) => {
-  console.log("使用者提問：", req.body.queryResult.queryText);
-  console.log("回覆：", req.body.queryResult.fulfillmentText);
-  console.log("使用Intents：", req.body.queryResult.intent.displayName);
-  //Socket回傳給首頁////////////////////////////////
-  //存入資料庫/////////////////////////////////////
-});
-/////////////檢查使用者(是否正確為使用者)////////////////////
-router.post(process.env.ROUTER_CHECKUSER, async (req, res) => {
-  SignUpTemplateCopy.findOne({ _id: req.body._id }, (err, result) => {
-    if (err) throw err;
-    if (result == null) {
-      res.send(false);
-    } else {
-      res.send(true);
-    }
+router.post(process.env.ROUTER_DIALOGFLOW, express.json(), (req, res, next) => {
+  const agent = new dialogflowAgent.WebhookClient({
+    request: req,
+    response: res,
   });
+  //test
+  function demo(agent) {
+    agent.add("Sending response from webhook server");
+  }
+
+  //Read Note
+  function Note_read(agent) {
+    console.log("使用者提問：", req.body.queryResult.queryText);
+    agent.add("Response from webhook server");
+    FindUserNote = new UserNoteTemplateCopy();
+    // FindUserNote.findOne(
+    //   {
+    //     StudentId: req.body.StudentId,
+    //   },
+    //   (err, result) => {
+    //     if (err) throw err;
+    //     agent.add("Fuck you");
+    //   }
+    // );
+  }
+
+  let intentMap = new Map();
+
+  intentMap.set("webhookDemo", demo);
+  intentMap.set("Menu_Note_Read_Find", Note_read);
+
+  agent.handleRequest(intentMap);
+  // console.log("使用者提問：", req.body.queryResult.queryText);
+  // console.log("回覆：", req.body.queryResult.fulfillmentText);
+  // console.log("使用Intents：", req.body.queryResult.intent.displayName);
 });
 /////////////取得使用者（除密碼）/////////////////////////
 router.post(process.env.ROUTER_USERINFO, async (req, res) => {
+  if (req.body._id == null) {
+    res.send(false);
+    return;
+  }
   SignUpTemplateCopy.findOne({ _id: req.body._id }, (err, result) => {
     if (err) throw err;
     const SendResponse = {
@@ -123,6 +148,101 @@ router.post(process.env.ROUTER_CHANGEEMAIL, async (req, res) => {
       res.send("success");
     }
   );
+});
+
+/////////////////管理員修改資料//////////////////
+router.post(process.env.ROUTER_ADMINEDIT, async (req, res) => {
+  if (req.body.Password === "") {
+    SignUpTemplateCopy.updateOne(
+      { Name: req.body.Name },
+      {
+        StudentId: req.body.StudentId,
+        Email: req.body.Email,
+        Access: req.body.Access,
+      },
+      (err, result) => {
+        if (err) throw err;
+        res.send("success");
+      }
+    );
+  } else {
+    bcryptjs.hash(req.body.Password, 10, (err, hashed) => {
+      SignUpTemplateCopy.updateOne(
+        { Name: req.body.Name },
+        {
+          StudentId: req.body.StudentId,
+          Email: req.body.Email,
+          Password: hashed,
+          Access: req.body.Access,
+        },
+        (err, result) => {
+          if (err) throw err;
+          res.send("success");
+        }
+      );
+    });
+  }
+});
+/////////////////管理員刪除資料//////////////////
+router.post(process.env.ROUTER_ADMINDELETED, async (req, res) => {
+  SignUpTemplateCopy.deleteOne({ Name: req.body.Name }, {}, (err, result) => {
+    if (err) throw err;
+    res.send("success");
+  });
+});
+/////////////////管理員批量修改//////////////////
+router.post(process.env.ROUTER_ADMINUPLOAD, async (req, res) => {
+  let InputData = req.body;
+
+  for (let i = 0; i < InputData.length; i++) {
+    if (req.body[i].Password === "") {
+      SignUpTemplateCopy.updateOne(
+        { Name: req.body[i].Name },
+        {
+          Name: req.body[i].Name,
+          StudentId: req.body[i].StudentId,
+          Access: req.body[i].Access,
+          Email: req.body[i].Email,
+        },
+        (err, result) => {
+          if (err) {
+            res.send("failed");
+            throw err;
+          }
+        }
+      );
+    } else {
+      bcryptjs.hash(req.body[i].Password, 10, (err, hashed) => {
+        SignUpTemplateCopy.updateOne(
+          { Name: req.body[i].Name },
+          {
+            Name: req.body[i].Name,
+            StudentId: req.body[i].StudentId,
+            Access: req.body[i].Access,
+            Email: req.body[i].Email,
+            Password: hashed,
+          },
+          (err, result) => {
+            if (err) {
+              res.send("failed");
+              throw err;
+            }
+            if (result.matchedCount === 0) {
+              let TempUpdate = new SignUpTemplateCopy({
+                Name: req.body[i].Name,
+                StudentId: req.body[i].StudentId,
+                Access: req.body[i].Access,
+                Email: req.body[i].Email,
+                Password: hashed,
+              });
+              TempUpdate.save();
+            }
+          }
+        );
+      });
+    }
+  }
+  res.send("success");
 });
 
 module.exports = router;
